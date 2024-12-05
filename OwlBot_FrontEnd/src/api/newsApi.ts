d825e601f1ea780a8b331c112a177d645a802ea0
vishalarvin <vishalarvinvinayagam98@gmail.com>
2024-12-05 13:43:16 -0500

    News api changes

--- OwlBot_FrontEnd/src/api/newsApi.ts
+++ OwlBot_FrontEnd/src/api/newsApi.ts
@@ -1,12 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
+import news from "../assets/news.json"
 
-const News_API = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3a728d526e5349dd89913977b65f8280";
+// const News_API = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3a728d526e5349dd89913977b65f8280";
 
 export const fetchNewsFromApi = async () => {
     try {
-        const response = await axios.get(News_API);
-        return response.data.articles;
-    } catch (error:any) {
+        // const response = await axios.get(News_API);
+        // return response.data.articles;
+        const response = news
+        return response.articles;
+    } catch (error: any) {
+        console.error("Error fetching news:", error.message);
         throw new Error(
             error.response?.data?.message || "Failed to fetch news"
         );
